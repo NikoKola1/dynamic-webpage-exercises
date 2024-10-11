21484652974d749b4e11dc2fe55e623cfec390b6
NikoKola1 <n3koni00@students.oamk.fi>
2024-10-11 12:35:37 +0300

    Routing added on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import CompAssignment from './componentAssignment.jsx';
+import { createBrowserRouter, Link, Navigate, RouterProvider, useNavigate } from 'react-router-dom';
+//import OpenDataAssignment from './OpenDataAssignment';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
 
-function App() {
-
-  return (
-    <div>
-      <CompAssignment/>
-    </div>
+function Home(){
+  return(
+    <>
+      <h2>Choose Assingment to check</h2>
+      <Link to='/componentassignment'>
+      <button>component assignment</button>
+      </Link>
+      <Link to='/opendataassignment'>
+      <button>opendata assignment coming soon</button>
+      </Link>
+    </>
   );
-};
+}
+
+const myRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/componentassignment',
+    element: <CompAssignment/>,
+  },
+ // {
+  //  path: '/opendataassignment',
+  //  element: <OpenDataAssignment/>
+ // };
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={myRouter}/>
+    </>
+  );
+}
 
 export default App
